51441fc42f9d5cd2feec86634116daaf217a24f2
Chaman Sarkar <84088334+Chaman1998@users.noreply.github.com>
2024-04-01 09:24:49 +0000

    Comparison Data Type

---
+++ basic/ComparisonDatatypes.js
@@ -0,0 +1,16 @@
+console.log("2">1);
+console.log("02">1);
+console.log("2"==2);
+
+console.log("Check NULL Type:- ");
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log("Check Undefined Type:- ");
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("Stricked check Type:- ");
+console.log("2" === 2, "(Here we check also data type here)");
