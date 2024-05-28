850c0f4f62d811820a69abacb9ff29e284e7f211
chaman1998 <chamansarkar11@gmail.com>
2024-05-28 17:50:28 +0530

    Auto background color change project done.

---
+++ basic/27.project/1-BackgroundColor/main.js
@@ -0,0 +1,34 @@
+// Generate a random Color
+
+//==========Create random color code
+const randomColor = function () {
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color;
+};
+
+console.log(randomColor());     //print random generated number
+let intervalId;
+
+//============function for start color change
+const startChangingColor = function () {
+    if(!intervalId){
+        intervalId = setInterval(changeBgColor, 1000);
+    }
+
+    function changeBgColor() {
+        document.body.style.backgroundColor = randomColor();
+    }
+};
+//=============function for stop color change
+const stopChangingColor = function () {
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+//===============Button Id Call
+document.querySelector('#start').addEventListener('click', startChangingColor);
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
