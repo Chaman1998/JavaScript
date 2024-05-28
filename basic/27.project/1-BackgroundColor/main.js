49d00275bbe2945d06f1f685de832893724b1bac
chaman1998 <chamansarkar11@gmail.com>
2024-05-28 18:09:00 +0530

    some code add on this project

--- basic/27.project/1-BackgroundColor/main.js
+++ basic/27.project/1-BackgroundColor/main.js
@@ -10,17 +10,18 @@ const randomColor = function () {
     return color;
 };
 
-console.log(randomColor());     //print random generated number
+// console.log(randomColor());     //print random generated number
 let intervalId;
 
 //============function for start color change
 const startChangingColor = function () {
     if(!intervalId){
-        intervalId = setInterval(changeBgColor, 1000);
+        intervalId = setInterval(changeBgColor, 100);
     }
 
     function changeBgColor() {
         document.body.style.backgroundColor = randomColor();
+        document.querySelector('#colorCode').innerHTML = randomColor();
     }
 };
 //=============function for stop color change
