477d8cb746c570cfbc0f266f55b2b580df45dea2
chaman1998 <chamansarkar11@gmail.com>
2024-06-05 22:36:43 +0530

    other parts on Promises

--- basic/27.Promise/promises.js
+++ basic/27.Promise/promises.js
@@ -13,3 +13,38 @@ const promiseOne = new Promise(function(resolve, reject){
 promiseOne.then(function(){
     console.log("Promise Consumed");
 })
+// =====================Another format=======================================
+new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        console.log("Async 2nd task");
+        resolve()
+    }, 2000);
+}).then( () => {
+    console.log("2nd Promise Consumed");
+})
+
+// =========================3rd Promises======================================
+const promiseThree = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve({username:'Chaman',email:'chaman@gmail.com'})
+    }, 3000)
+})
+promiseThree.then((user)=>{
+    console.log(user);
+    console.log(user.username);
+})
+// =========================4th Promises======================================
+const promiseFour = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let error = true;
+        if(!error){
+            resolve({username:'Chaman',password:'1234'})
+        }else{
+            reject('ERROR: Something went Wrong')
+        }
+    }, 4000)
+})
+const username = promiseFour.then((user)=>{
+    return user.username;
+    // console.log(user.username);
+})
