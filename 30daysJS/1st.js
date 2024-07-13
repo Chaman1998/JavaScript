bd78cbaf0f7574e73133e26230aeb2ac21bcf774
chaman1998 <chamansarkar11@gmail.com>
2024-07-13 18:47:12 +0530

    30days js part

---
+++ 30daysJS/1st.js
@@ -0,0 +1,29 @@
+//variable declaration
+var v = 1;
+console.log(v);
+
+let l = 'js';
+console.log(l);
+
+//constant Declaration
+const c = true;
+console.log(c);
+
+//datatypes
+console.log(typeof (v));
+console.log(typeof (l));
+console.log(typeof (c));
+
+//reassigning variables
+let l2 = 0;
+l2 = 'o'
+console.log(l2);
+
+//understand const
+c = 's'
+// console.log(c); 
+/*
+this 'console.log' show error because,
+The value of a constant can't be changed through 
+reassignment using the assignment operator
+ */
