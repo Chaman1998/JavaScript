86091bc7ff6bb9e57a14bbe8afe070d49ff881df
chaman1998 <chamansarkar11@gmail.com>
2024-07-14 22:31:27 +0530

    6th

---
+++ practice/6.js
@@ -0,0 +1,10 @@
+/*
+Exercise 1:
+Write a JavaScript program to create a new string from a given string taking the last 3
+characters and added at both the front and back. The string length must be 3 or more, if
+not, the original string is returned.
+*/
+const strCheck = (str) =>
+    str.length < 3 ? str : str.slice(0,3) + str.slice(-3);
+
+console.log(strCheck("Wri"));
