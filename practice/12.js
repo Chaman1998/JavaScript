4e6ea8f8b6f6ca3365a07daf9101ddaf84de8320
chaman1998 <chamansarkar11@gmail.com>
2024-07-14 23:59:01 +0530

    12th

---
+++ practice/12.js
@@ -0,0 +1,20 @@
+/*
+Exercise 12:
+
+Write a JavaScript program to find the number of 
+even values up to a given number
+*/
+
+const countEventNumbers = (arr) => 
+    arr.filter(num => num % 2 ===0).length;
+
+const createArrayOfNumbers = (num) => {
+    const returnArray = [];
+    for (let i = 0; i < num; i++) {
+        returnArray.push(i);
+    }
+    return returnArray;
+};
+
+console.log(countEventNumbers(createArrayOfNumbers(6)));
+console.log(countEventNumbers(createArrayOfNumbers(9)));
